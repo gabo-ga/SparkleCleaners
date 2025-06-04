5141ecef4d4675d60e9e940b571a4878c7064d11
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-06-03 23:23:56 -0400

    hero responsive

--- components/Hero/benefits.tsx
+++ components/Hero/benefits.tsx
@@ -3,26 +3,27 @@
 export default function Benefits() {
     return (
         <section className="container mx-auto p-4">
-                <div className="grid grid-cols-2 gap-2">
-                    {/* Grid Item 1 */}
-                    <div className="bg-white p-2">
-                        <h3 className="text-md font-bold mb-4">Certified Deep Cleaning</h3>
-                    </div>
-                    {/* Grid Item 2 */}
-                    <div className="bg-white p-2">
-                        <h3 className="text-md font-bold mb-4">Quick Online Booking</h3>
-                    </div>
-
-                    {/* Grid Item 3 */}
-                    <div className="bg-white p-2">
-                        <h3 className="text-md font-bold mb-4">Safe Booking System</h3>
-                    </div>
-
-                    {/* Grid Item 4 */}
-                    <div className="bg-white p-2">
-                        <h3 className="text-md font-bold mb-4">Trustworthy Professionals</h3>
-                    </div>
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4">
+                {/* Grid Item 1 */}
+                <div className="bg-white p-2 lg:p-4">
+                    <h3 className="text-md lg:text-lg font-bold mb-2 lg:mb-4">Certified Deep Cleaning</h3>
                 </div>
-            </section>
+                
+                {/* Grid Item 2 */}
+                <div className="bg-white p-2 lg:p-4">
+                    <h3 className="text-md lg:text-lg font-bold mb-2 lg:mb-4">Quick Online Booking</h3>
+                </div>
+
+                {/* Grid Item 3 */}
+                <div className="bg-white p-2 lg:p-4">
+                    <h3 className="text-md lg:text-lg font-bold mb-2 lg:mb-4">Safe Booking System</h3>
+                </div>
+
+                {/* Grid Item 4 */}
+                <div className="bg-white p-2 lg:p-4">
+                    <h3 className="text-md lg:text-lg font-bold mb-2 lg:mb-4">Trustworthy Professionals</h3>
+                </div>
+            </div>
+        </section>
     );
 }
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -9,7 +9,7 @@ import Benefits from './benefits';
 export default function Hero() {
     return (
         <>
-            <section className="relative w-full h-[50vh]  sm:h-[60vh] md:h-[70vh] lg:h-[80vh] overflow-hidden">
+            <section className="relative w-full h-[50vh]  sm:h-[60vh] md:h-[70vh] lg:h-[70vh] overflow-hidden">
                 <Image
                     src="/background.webp"
                     alt="Sparkle Cleaners Background"
@@ -19,9 +19,9 @@ export default function Hero() {
                     quality={100}
                     className="object-cover object-top filter brightness-100 opacity-60 lg:object-cover"
                 />
-                <div className="absolute flex flex-col items-center justify-center text-black p-7 pr-34 sm:p-10 sm:pr-38 sm:pt-14 md:pr-64">
+                <div className="absolute flex flex-col items-center justify-center text-black p-7 pr-34 sm:p-10 sm:pr-38 sm:pt-14 md:pr-64 lg:pr-74 lg:pt-26 lg:pl-16">
                     <h1 className="text-xl sm:text-4xl lg:text-6xl font-bold mb-4">EXPERT CLEANING AT YOUR DOORSTEP</h1>
-                    <p className="text-xs sm:text-xl mb-6">Hi! we are a professional cleaning company offering personalized cleaning services</p>
+                    <p className="text-xs sm:text-xl mb-6 lg:text-3xl">Hi! we are a professional cleaning company offering personalized cleaning services</p>
                     <div className='flex items-start justify-start w-full'>
                         <BookNowButton/>
                     </div>
