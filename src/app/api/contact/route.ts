88621751a7f3c2553ffad08f5065f07069b64ad8
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-07-03 18:46:32 -0400

    feat:mail send feature

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendMail } from "@/lib/mail";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
@@ -5,7 +6,6 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
     try {
       const body = await req.json();
-  
       const { name, email, telephone, messageContent } = body;
   
       let customer = await prisma.customer.findUnique({
@@ -15,10 +15,7 @@ export async function POST(req: Request) {
       // If not, create the customer
       if (!customer) {
         customer = await prisma.customer.create({
-          data: {
-            name,
-            email,
-            telephone,
+          data: { name, email, telephone,
           },
         });
       }
@@ -29,6 +26,18 @@ export async function POST(req: Request) {
           customerId: customer.id,
         },
       });
+
+      //send email
+      await sendMail(
+        process.env.EMAIL_TO as string,
+        "New contact form submission",
+        `
+        Name: ${name}\n
+        Email: ${email}\n
+        Telephone: ${telephone}\n
+        Message: ${messageContent}
+        `.trim()
+      );
   
       return NextResponse.json({ customer, message }, { status: 201 });
   
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,15 +3,31 @@ import nodemailer from 'nodemailer';
 if (!process.env.SMTP_HOST ||
     !process.env.SMTP_PORT ||
     !process.env.SMTP_USER ||
-    !process.env.SMTP_PASS) {
+    !process.env.SMTP_PSWRD) {
   throw new Error('Missing SMTP configuration in .env');
 }
 
+  
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: process.env.SMTP_PSWRD,
   },
 });
+
+export async function sendMail(to: string, subject: string, text: string) {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      text,
+    });
+    console.log('Message sent:', info.messageId);
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+}
